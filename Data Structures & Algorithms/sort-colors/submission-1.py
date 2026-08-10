b49c038fa9f80a85e0dfaeed4d26faa4a9733249
PUNEET MISHRA <167070714+mpuneet063@users.noreply.github.com>
2026-08-10 15:43:31 +0100

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # THIS PROBLEM IS SOLVED USING QUICKSORT PARTITION
+        l, r = 0, len(nums)-1
+        i = 0
+        # def swap(i, j):
+        #     nums[i] = nums[j]
+        #     nums[j] = nums[i]
+        while i<= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+                i -= 1
+            i += 1
